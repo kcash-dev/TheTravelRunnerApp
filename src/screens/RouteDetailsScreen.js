f4abe006557672547586d127a92f9302d4c10e7b
Kyle Cash <kcashdev@gmail.com>
2021-12-21 17:58:26 -0500

    setting up route details page

--- src/screens/RouteDetailsScreen.js
+++ src/screens/RouteDetailsScreen.js
@@ -1,17 +1,64 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import { StyleSheet, Text, View, ScrollView, Image, Pressable, Linking } from 'react-native'
 import Maps from '../components/Maps';
 import tailwind from 'tailwind-rn';
+import { FontAwesome5 } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+//Components
+import OwnStatusBar from '../components/StatusBar';
+
+function useForceUpdate(){
+    const [value, setValue] = useState(0); // integer state
+    return () => setValue(value => value + 1); // update the state to force render
+}
 
 const RouteDetailsScreen = ({ route }) => {
     const { data } = route.params;
 
+    const forceUpdate = useForceUpdate();
+
     console.log(data)
 
     return (
-        <View style={ tailwind(`flex-1`) }>
+        <View style={ tailwind(`flex-1 bg-white`) }>
+            <OwnStatusBar backgroundColor="#003b36" />
             <View style={ tailwind(`h-1/2`) }>
-                <Maps origin={ data.origin } />
+                <Maps origin={ data.origin } update={ forceUpdate } json={ data.jsonData } />
+            </View>
+            <View style={ tailwind(`h-1/2`) }>
+                <View style={ tailwind(`w-full h-20 border-b justify-center`) }>
+                    <Text style={ tailwind(`font-bold text-xl pl-3`) }>{ data.name }</Text>
+                </View>
+                <View style={ tailwind(`flex-row w-full border-b`) }>
+                    <View style={ tailwind(`w-1/2 justify-center items-center h-16 flex-row justify-evenly border-r`) }>
+                        <Text style={ tailwind(`font-bold text-xl pl-3`) }>{ data.distance } km</Text>
+                        <MaterialCommunityIcons name="map-marker-distance" size={30} color="black" style={ tailwind(`px-7`) } />
+                    </View>
+                    <View style={ tailwind(`w-1/2 justify-center items-center h-16 flex-row justify-evenly`) }>
+                        <Text style={ tailwind(`font-bold text-xl pl-3`) }>{ data.elevation } m</Text>
+                        <FontAwesome5 name="mountain" size={24} color="black" style={ tailwind(`px-7 pb-3 top-2`) } />
+                    </View>
+                </View>
+                <View style={ tailwind(`items-center h-20 w-full`) } >
+                    <Image
+                        source={{ uri: data.elevationMap }}
+                        style={ tailwind(`h-16 w-full`) }
+                    />
+                </View>
+                <View style={ tailwind(`h-20 items-center`) }>
+                    <Pressable
+                        style={({ pressed }) => [{
+                                opacity: pressed ? 0.5 : 1
+                            },
+                            tailwind(`bg-green-900 rounded-lg items-center justify-center`),
+                            styles.shadow
+                        ]}
+                        onPress={() => Linking.openURL(data.gpxUrl) }
+                    >
+                        <Text style={ tailwind(`px-10 py-5 text-white font-bold`) }>Download route</Text>
+                    </Pressable>
+                </View>
             </View>
         </View>
     )
@@ -19,4 +66,16 @@ const RouteDetailsScreen = ({ route }) => {
 
 export default RouteDetailsScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    shadow: {
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 3,
+        },
+        shadowOpacity: 0.29,
+        shadowRadius: 4.65,
+
+        elevation: 7,
+    }
+})
