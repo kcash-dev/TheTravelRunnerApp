eee8c8f861400bb1b3ba0ce77c3516b798212558
Kyle Cash <kcashdev@gmail.com>
2021-12-31 15:07:40 -0500

    fixed header spacing

--- src/components/RSSReader.js
+++ src/components/RSSReader.js
@@ -144,7 +144,7 @@ const RSSReader = ({ rssFeed }) => {
 
     const { width } = useWindowDimensions();
     const HEADER_EXPANDED_HEIGHT = 250
-    const HEADER_COLLAPSED_HEIGHT = 80
+    const HEADER_COLLAPSED_HEIGHT = 120
     const headerTitleOpacity = state.scrollY.interpolate({
         inputRange: [0, HEADER_EXPANDED_HEIGHT - HEADER_COLLAPSED_HEIGHT],
         outputRange: [0, 1],
@@ -174,18 +174,18 @@ const RSSReader = ({ rssFeed }) => {
                     <View style={ tailwind(`flex-1 bg-black bg-opacity-40 items-center w-full`) }>
                         <View style={ tailwind(`flex-row justify-evenly items-center`) }>
                             <Animated.Image 
-                                style={[ tailwind(`mt-5 h-10 w-10 rounded-lg`), { opacity: headerTitleOpacity} ]}
+                                style={[ tailwind(`mt-12 h-14 w-14 rounded-lg`), { opacity: headerTitleOpacity} ]}
                                 source={{ uri: 'https://i.imgur.com/smHitpL.png' }}
                             />
-                            <View>
+                            <View style={ tailwind(`mt-8`) }>
                                 <Animated.Text style={[ tailwind(`mt-5 px-10 text-center font-bold text-white text-opacity-100 italic text-2xl`), { opacity: headerTitleOpacity } ]}>The Travel Runner</Animated.Text>
                                 <Animated.Text style={[ tailwind(`text-center text-white text-opacity-90 text-xs`), { opacity: headerTitleOpacity} ]}>Where Travel and Trail Running Collide</Animated.Text>
                             </View>
                         </View>
-                        <Animated.Text style={[ tailwind(`absolute bottom-20 mb-2 text-4xl font-bold text-white text-opacity-90 italic`), { opacity: heroTitleOpacity} ]}>The Travel Runner</Animated.Text>
-                        <Animated.Text style={[ tailwind(`absolute bottom-16 px-2 text-center text-white text-opacity-90`), { opacity: heroTitleOpacity} ]}>Where Travel and Trail Running Collide</Animated.Text>
+                        <Animated.Text style={[ tailwind(`absolute bottom-16 mb-2 text-4xl font-bold text-white text-opacity-90 italic`), { opacity: heroTitleOpacity} ]}>The Travel Runner</Animated.Text>
+                        <Animated.Text style={[ tailwind(`absolute bottom-14 px-2 text-center text-white text-opacity-90`), { opacity: heroTitleOpacity} ]}>Where Travel and Trail Running Collide</Animated.Text>
                         <Animated.Image 
-                            style={[ tailwind(`absolute rounded-lg top-6 left-2 h-20 w-20`), { opacity: heroTitleOpacity} ]}
+                            style={[ tailwind(`absolute rounded-lg top-10 left-2 h-20 w-20`), { opacity: heroTitleOpacity} ]}
                             source={{ uri: 'https://i.imgur.com/smHitpL.png' }}
                         />
                     </View>
